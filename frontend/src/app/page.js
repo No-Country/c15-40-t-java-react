2d5cb2013e8bb8b0aef0bcc0bb0dd4a3c8fe6959
Fabian Ochoa <cafaruoc2000@hotmail.com>
2023-11-28 11:34:00 -0500

    .

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,6 +4,7 @@ export default function Home () {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Educ.Ar</h1>
+      <p>thefabi8a</p>
     </main>
   );
 }
