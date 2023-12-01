8b075d34a8a82c55f63225fe96c3613a4a25d427
Cristian Mozetich <cristianmozetich@gmail.com>
2023-12-01 18:58:18 +0000

    actualizaciones login

--- frontend/src/components/formLogin/FormLogin.jsx
+++ frontend/src/components/formLogin/FormLogin.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import {Input} from "@nextui-org/react";
 import { Index } from '.';
+import Link from 'next/link';
+
+
 
 
 const FormLogin = () => {
   const {handleFormSubmit, user, password, setPassword, setUser} = Index()
 
+
   return (
-        <form onSubmit={handleFormSubmit} className='max-w-md mx-auto my-20 border rounded-md p-2'>
+        <form onSubmit={handleFormSubmit} className='max-w-md mx-auto my-20 border bg-white rounded-3xl p-4 shadow-xl'>
         <div className='mb-6 p-2'>
             <Input
             key='usuario'
             type="text"
             label="Usuario"
-            labelPlacement='Usuario'
+            labelPlacement="inside"
             placeholder="Nombre de Usuario"
             onchange={(e) => setUser(e.target.value)}
+            classNames={{
+                label: "font-semibold",
+            }}
             />
         </div>
         <div className='mb-6 p-2'>
@@ -23,18 +30,26 @@ const FormLogin = () => {
             key='contraseña'
             type="password"
             label="Contraseña"
-            labelPlacement='Contraseña'
+            labelPlacement="inside"
             placeholder="Ingrese Contraseña"
             onchange={(e) => setPassword(e.target.value)}
+            classNames={{
+                label: "font-semibold",
+            }}
             />
         </div>
         <div className='m-auto flex justify-center'>
-            <button type='submit' className='w-20 bg-blue-500 text-white font-semibold py-2 px-auto rounded-md hover:-bg-blue-600 focus:outline-none focus:bg-blue-600'>Ingresar</button>
+            <button type='submit' className='w-full bg-blue-500 text-white font-semibold py-2 px-auto rounded-md hover:-bg-blue-600 focus:outline-none focus:bg-blue-600'>Ingresar</button>
         </div>
-        <div className="mb-6 p-2 text-center">
-            <a href="#" className="text-blue-500 hover:underline">
+        <div className='text-center mt-4 p-2'>
+            <Link href={'/registrar-colegio'} className="text-blue-500 hover:underline">
+            ¿Todavía no estas registrado? Crea tu cuenta aqui
+            </Link>    
+        </div>
+        <div className="mb-6 text-center">
+            <Link href="#" className="text-blue-500 hover:underline">
             ¿Olvidaste tu contraseña?
-            </a>
+            </Link>
         </div>
     </form>
   )
