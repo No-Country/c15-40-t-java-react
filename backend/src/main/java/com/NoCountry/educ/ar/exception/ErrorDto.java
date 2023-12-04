87243cbe15d43c9cd85f38e5ad0bdd1b28afd6f6
RafaelTalavera <rtalavera@live.com.ar>
2023-12-04 16:15:35 -0300

    refactor user and validation

--- backend/src/main/java/com/NoCountry/educ/ar/exception/ErrorDto.java
+++
@@ -1,8 +0,0 @@
-package com.NoCountry.educ.ar.exception;
-
-public record ErrorDto(
-        String objectName,
-        String fieldName,
-        String rejectedValue,
-        String errorMessage
-) {}
