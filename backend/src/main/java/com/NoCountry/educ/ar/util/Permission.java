6b2a4ba458fa827d4038fb71035219aab4fecc9c
RafaelTalavera <rtalavera@live.com.ar>
2023-12-05 17:25:50 -0300

    create roles and permission

--- backend/src/main/java/com/NoCountry/educ/ar/util/Permission.java
+++ backend/src/main/java/com/NoCountry/educ/ar/util/Permission.java
@@ -1,2 +1,7 @@
-package com.NoCountry.educ.ar.util;public enum Permission {
+package com.NoCountry.educ.ar.util;
+
+public enum Permission {
+    READ_ALL_INSTITUTIONS,
+    SAVE_ONE_INSTITUTION;
 }
+
--- backend/src/main/java/com/NoCountry/educ/ar/util/Role.java
+++ backend/src/main/java/com/NoCountry/educ/ar/util/Role.java
@@ -1,2 +1,25 @@
-package com.NoCountry.educ.ar.util;public enum Role {
+package com.NoCountry.educ.ar.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Role {
+
+    READER(Arrays.asList(Permission.READ_ALL_INSTITUTIONS)),
+    ADMINISTRATOR(Arrays.asList(Permission.SAVE_ONE_INSTITUTION,Permission.READ_ALL_INSTITUTIONS));
+
+    private List<Permission> permissions;
+
+    Role(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
 }
