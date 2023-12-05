2924179abb8ac5c098cfa74d51e6914eea42c21d
RafaelTalavera <rtalavera@live.com.ar>
2023-12-05 17:17:51 -0300

    Define endpoints to obtain user information and register new users

--- backend/src/main/java/com/NoCountry/educ/ar/controller/AuthenticationController.java
+++ backend/src/main/java/com/NoCountry/educ/ar/controller/AuthenticationController.java
@@ -1,2 +1,33 @@
-package com.NoCountry.educ.ar.controller;public class AuthenticationController {
+package com.NoCountry.educ.ar.controller;
+
+import com.NoCountry.educ.ar.dto.AuthenticationRequest;
+import com.NoCountry.educ.ar.dto.AuthenticationResponse;
+import com.NoCountry.educ.ar.service.AuthenticationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PreAuthorize("permitAll")
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody @Valid AuthenticationRequest authRequest){
+        AuthenticationResponse jwtDto = authenticationService.login(authRequest);
+        return ResponseEntity.ok(jwtDto);
+    }
+
+    @PreAuthorize("permitAll")
+    @GetMapping("/public-access")
+    public String publicAccessEndpoint(){
+        return "este endpoint es público";
+    }
+
 }
