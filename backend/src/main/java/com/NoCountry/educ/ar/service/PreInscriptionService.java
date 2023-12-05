a52f322aed0c37cd9e69ea0ab716077c1ab1cbad
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-12-05 12:02:33 -0300

    Create pre inscription service

---
+++ backend/src/main/java/com/NoCountry/educ/ar/service/PreInscriptionService.java
@@ -0,0 +1,15 @@
+package com.NoCountry.educ.ar.service;
+
+import java.util.List;
+
+import com.NoCountry.educ.ar.dto.PreInscriptionRequestDTO;
+import com.NoCountry.educ.ar.entity.PreInscription;
+
+public interface PreInscriptionService {
+    
+    public PreInscription createPreInscription(PreInscriptionRequestDTO preInscriptionDTO);
+
+    public PreInscription getPreInscriptionById(String id);
+
+    public List<PreInscription> getPreInscriptions();
+}
