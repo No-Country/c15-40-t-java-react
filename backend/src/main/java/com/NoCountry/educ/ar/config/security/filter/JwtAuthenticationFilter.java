1cc30a61f7cdd87c0a51e7545b722740254a7b12
RafaelTalavera <rtalavera@live.com.ar>
2023-12-05 17:16:39 -0300

    Handles authentication requests and returns a JWT token

--- backend/src/main/java/com/NoCountry/educ/ar/config/security/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/NoCountry/educ/ar/config/security/filter/JwtAuthenticationFilter.java
@@ -1,2 +1,57 @@
-package com.NoCountry.educ.ar.config.security.filter;public class JwtAuthenticationFilter {
+package com.NoCountry.educ.ar.config.security.filter;
+
+import com.NoCountry.educ.ar.entity.User;
+import com.NoCountry.educ.ar.repository.UserRepository;
+import com.NoCountry.educ.ar.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        //1. Obtener el header que contiene el jwt
+        String authHeader = request.getHeader("Authorization"); // Bearer jwt
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //2. Obtener jwt desde header
+        String jwt = authHeader.split(" ")[1];
+
+        //3. Obtener subject/username desde el jwt
+        String username = jwtService.extractUsername(jwt);
+
+        //4. Setear un objeto Authentication dentro del SecurityContext
+
+        User user = userRepository.findByUsername(username).get();
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                username, null, user.getAuthorities()
+        );
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        //5. Ejecutar el resto de filtros
+
+        filterChain.doFilter(request, response);
+    }
 }
