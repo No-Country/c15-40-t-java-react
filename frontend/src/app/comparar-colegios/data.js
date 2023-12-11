d200bcde200358c148e2242fa52cd608a5f13076
diegopaff <110573986+diegopaff@users.noreply.github.com>
2023-12-11 17:16:30 -0300

    update merge frontend comparar-page

--- frontend/src/app/comparar-colegios/data.js
+++ frontend/src/app/comparar-colegios/data.js
@@ -1,188 +1,188 @@
 export const colegios = [
   {
-    label: "Colegio1",
-    value: "Colegio1",
-    description: "Colegio1 es el mejor",
+    label: 'Colegio1',
+    value: 'Colegio1',
+    description: 'Colegio1 es el mejor'
   },
   {
-    label: "Colegio2",
-    value: "Colegio2",
-    description: "Colegio2 es el mejor",
+    label: 'Colegio2',
+    value: 'Colegio2',
+    description: 'Colegio2 es el mejor'
   },
   {
-    label: "Colegio3",
-    value: "Colegio3",
-    description: "Colegio3 es el mejor",
+    label: 'Colegio3',
+    value: 'Colegio3',
+    description: 'Colegio3 es el mejor'
   },
   {
-    label: "Colegio4",
-    value: "Colegio4",
-    description: "Colegio4 es el mejor",
-  },
+    label: 'Colegio4',
+    value: 'Colegio4',
+    description: 'Colegio4 es el mejor'
+  }
 ];
 
 export const colegioDetail = [
   {
-    id: "Colegio1",
-    logo:"https://png.pngtree.com/png-clipart/20220928/original/pngtree-nutrition-school-health-coaches-logo-png-image_8639850.png",
-    thumbnail: "https://images.pexels.com/photos/207692/pexels-photo-207692.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    id: 'Colegio1',
+    logo: 'https://png.pngtree.com/png-clipart/20220928/original/pngtree-nutrition-school-health-coaches-logo-png-image_8639850.png',
+    thumbnail: 'https://images.pexels.com/photos/207692/pexels-photo-207692.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     pre_inscription_id: 1, // Referencia al documento en Pre_Inscription
-    gestion: "public",
-    institutionName: "Lopa Colegio",
-    address: "Lopa Lopa",
-    city: "Montevideo",
+    gestion: 'public',
+    institutionName: 'Lopa Colegio',
+    address: 'Lopa Lopa',
+    city: 'Montevideo',
     phones: [],
-    cue: "cue",
-    web: "google.com.ar",
+    cue: 'cue',
+    web: 'google.com.ar',
     educationLevels: [
       {
-        name: "kindergarten",
+        name: 'kindergarten',
         fee: { min: 20000, max: 25000 },
-        inscriptionDate: "30/05/1989",
+        inscriptionDate: '30/05/1989',
         shifts: [
-          { name: "turno mañana", schedule: { from: 8, to: 12 } },
-          { name: "turno tarde", schedule: { from: 13, to: 17 } },
-        ],
+          { name: 'turno mañana', schedule: { from: 8, to: 12 } },
+          { name: 'turno tarde', schedule: { from: 13, to: 17 } }
+        ]
       },
       {
-        name: "highschool",
+        name: 'highschool',
         fee: { min: 30000, max: 45000 },
-        inscriptionDate: "30/05/1989",
+        inscriptionDate: '30/05/1989',
         shifts: [
-          { name: "turno mañana", schedule: { from: 8, to: 12 } },
-          { name: "turno tarde", schedule: { from: 13, to: 17 } },
+          { name: 'turno mañana', schedule: { from: 8, to: 12 } },
+          { name: 'turno tarde', schedule: { from: 13, to: 17 } }
         ],
-        orientations: ["Economics", "Arts", "Chemistry"],
-      },
+        orientations: ['Economics', 'Arts', 'Chemistry']
+      }
     ],
-    talleres: ["Voley", "Ajedrez"],
+    talleres: ['Voley', 'Ajedrez'],
     bilingual: false,
     comedor: false,
-    religion: ["Catolico", "Judio"],
+    religion: ['Catolico', 'Judio'],
     uniforme: false,
-    genere: "Género",
-    condicionesEdilicias: "Condiciones Edilicias",
-    calefaccion: false,
+    genere: 'Género',
+    condicionesEdilicias: 'Condiciones Edilicias',
+    calefaccion: false
   },
   {
-    id: "Colegio2",
-    logo:"https://png.pngtree.com/png-clipart/20220928/original/pngtree-nutrition-school-health-coaches-logo-png-image_8639850.png",
-    thumbnail: "https://images.pexels.com/photos/207684/pexels-photo-207684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    id: 'Colegio2',
+    logo: 'https://png.pngtree.com/png-clipart/20220928/original/pngtree-nutrition-school-health-coaches-logo-png-image_8639850.png',
+    thumbnail: 'https://images.pexels.com/photos/207684/pexels-photo-207684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     pre_inscription_id: 2, // Referencia al documento en Pre_Inscription
-    web: "google.com.ar",
-    gestion: "public",
-    institutionName: "Ilis",
-    address: "Ilis Ilis 55",
-    city: "Montevideo",
+    web: 'google.com.ar',
+    gestion: 'public',
+    institutionName: 'Ilis',
+    address: 'Ilis Ilis 55',
+    city: 'Montevideo',
     phones: [],
-    cue: "cue",
+    cue: 'cue',
     educationLevels: [
       {
-        name: "kindergarten",
+        name: 'kindergarten',
         fee: { min: 20000, max: 25000 },
-        inscriptionDate: "15/06/1955",
+        inscriptionDate: '15/06/1955',
         shifts: [
-          { name: "turno mañana", schedule: { from: 8, to: 12 } },
-          { name: "turno tarde", schedule: { from: 13, to: 17 } },
-        ],
+          { name: 'turno mañana', schedule: { from: 8, to: 12 } },
+          { name: 'turno tarde', schedule: { from: 13, to: 17 } }
+        ]
       },
       {
-        name: "highschool",
+        name: 'highschool',
         fee: { min: 30000, max: 45000 },
-        inscriptionDate: "30/05/1989",
+        inscriptionDate: '30/05/1989',
         shifts: [
-          { name: "turno mañana", schedule: { from: 8, to: 12 } },
-          { name: "turno tarde", schedule: { from: 13, to: 17 } },
+          { name: 'turno mañana', schedule: { from: 8, to: 12 } },
+          { name: 'turno tarde', schedule: { from: 13, to: 17 } }
         ],
-        orientations: ["Economics", "Arts", "Chemistry"],
-      },
+        orientations: ['Economics', 'Arts', 'Chemistry']
+      }
     ],
-    talleres: ["Voley", "Ajedrez"],
+    talleres: ['Voley', 'Ajedrez'],
     bilingual: true,
     comedor: true,
-    religion: ["Catolico", "Judio"],
+    religion: ['Catolico', 'Judio'],
     uniforme: true,
-    genere: "Género",
-    condicionesEdilicias: "Condiciones Edilicias",
-    calefaccion: true,
+    genere: 'Género',
+    condicionesEdilicias: 'Condiciones Edilicias',
+    calefaccion: true
   },
   {
-    id: "Colegio3",
-    logo:"https://png.pngtree.com/png-clipart/20220928/original/pngtree-nutrition-school-health-coaches-logo-png-image_8639850.png",
-    thumbnail:"https://images.pexels.com/photos/2305098/pexels-photo-2305098.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    id: 'Colegio3',
+    logo: 'https://png.pngtree.com/png-clipart/20220928/original/pngtree-nutrition-school-health-coaches-logo-png-image_8639850.png',
+    thumbnail: 'https://images.pexels.com/photos/2305098/pexels-photo-2305098.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     pre_inscription_id: 2, // Referencia al documento en Pre_Inscription
-    web: "google.com.ar",
-    gestion: "public",
-    institutionName: "Colegio Ingles",
-    address: "Ilis Ilis 55",
-    city: "Buenos Aires",
+    web: 'google.com.ar',
+    gestion: 'public',
+    institutionName: 'Colegio Ingles',
+    address: 'Ilis Ilis 55',
+    city: 'Buenos Aires',
     phones: [],
-    cue: "cue",
+    cue: 'cue',
     educationLevels: [
       {
-        name: "kindergarten",
+        name: 'kindergarten',
         fee: { min: 20000, max: 25000 },
-        inscriptionDate: "15/06/1955",
+        inscriptionDate: '15/06/1955',
         shifts: [
-          { name: "turno mañana", schedule: { from: 8, to: 12 } },
-          { name: "turno tarde", schedule: { from: 13, to: 17 } },
-        ],
-      },
+          { name: 'turno mañana', schedule: { from: 8, to: 12 } },
+          { name: 'turno tarde', schedule: { from: 13, to: 17 } }
+        ]
+      }
     ],
-    talleres: ["Voley", "Ajedrez", "Fútbol", "Atletismo"],
+    talleres: ['Voley', 'Ajedrez', 'Fútbol', 'Atletismo'],
     bilingual: true,
     comedor: true,
-    religion: ["Catolico", "Judio"],
+    religion: ['Catolico', 'Judio'],
     uniforme: true,
-    genere: "Género",
-    condicionesEdilicias: "Condiciones Edilicias",
-    calefaccion: true,
+    genere: 'Género',
+    condicionesEdilicias: 'Condiciones Edilicias',
+    calefaccion: true
   },
   {
-    id: "Colegio4",
-    logo:"https://png.pngtree.com/png-clipart/20220928/original/pngtree-nutrition-school-health-coaches-logo-png-image_8639850.png",
-    thumbnail:"https://images.pexels.com/photos/989932/pexels-photo-989932.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    id: 'Colegio4',
+    logo: 'https://png.pngtree.com/png-clipart/20220928/original/pngtree-nutrition-school-health-coaches-logo-png-image_8639850.png',
+    thumbnail: 'https://images.pexels.com/photos/989932/pexels-photo-989932.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     pre_inscription_id: 2, // Referencia al documento en Pre_Inscription
-    web: "google.com.ar",
-    gestion: "public",
-    institutionName: "Colegio Frances",
-    address: "Maria Mieres 2565",
-    city: "Cordoba",
+    web: 'google.com.ar',
+    gestion: 'public',
+    institutionName: 'Colegio Frances',
+    address: 'Maria Mieres 2565',
+    city: 'Cordoba',
     phones: [],
-    cue: "cue",
+    cue: 'cue',
     educationLevels: [
       {
-        name: "highschool",
+        name: 'highschool',
         fee: { min: 30000, max: 45000 },
-        inscriptionDate: "30/05/1989",
+        inscriptionDate: '30/05/1989',
         shifts: [
-          { name: "turno mañana", schedule: { from: 8, to: 12 } },
-          { name: "turno tarde", schedule: { from: 13, to: 17 } },
+          { name: 'turno mañana', schedule: { from: 8, to: 12 } },
+          { name: 'turno tarde', schedule: { from: 13, to: 17 } }
         ],
-        orientations: ["Economics", "Arts", "Chemistry"],
-      },
+        orientations: ['Economics', 'Arts', 'Chemistry']
+      }
     ],
     talleres: [],
     bilingual: true,
     comedor: true,
-    religion: ["Catolico", "Judio"],
+    religion: ['Catolico', 'Judio'],
     uniforme: true,
-    genere: "Género",
-    condicionesEdilicias: "Condiciones Edilicias",
-    calefaccion: true,
-  },
+    genere: 'Género',
+    condicionesEdilicias: 'Condiciones Edilicias',
+    calefaccion: true
+  }
 ];
 
-////////////////////////////////
+/// /////////////////////////////
 const ciudadesNoRepeat = [...new Set(colegioDetail.map((colegio) => {
   return colegio.city;
-} ))];
+}))];
 
 export const ciudades = ciudadesNoRepeat.map((ciudad) => {
   return {
     label: ciudad,
     value: ciudad,
-    description: ciudad,
+    description: ciudad
   };
-} );
-///////////////////////////////
+});
+/// ////////////////////////////
--- frontend/src/components/compareCard/index.js
+++ frontend/src/components/compareCard/index.js
@@ -1,34 +1,34 @@
-import { colegioDetail } from "@/app/comparar-colegios/data";
+import { colegioDetail } from '@/app/comparar-colegios/data';
 
-export function index(colegioId) {
-  function filterObjectById(array, id) {
+export function index (colegioId) {
+  function filterObjectById (array, id) {
     return array.filter((objeto) => objeto.id === id)[0];
   }
 
   const colegio = filterObjectById(colegioDetail, colegioId);
-  let educationLevelsString = "";
-  let talleresString = "";
-  let religionesString = "";
+  let educationLevelsString = '';
+  let talleresString = '';
+  let religionesString = '';
   const institutionName = colegio.institutionName;
   const address = colegio.address;
   const city = colegio.city;
-  const bilingual = colegio.bilingual ? "Si" : "No";
-  const comedor = colegio.comedor ? "Si" : "No";
-  const uniforme = colegio.uniforme ? "Si" : "No";
-  const calefaccion = colegio.calefaccion ? "Si" : "No";
+  const bilingual = colegio.bilingual ? 'Si' : 'No';
+  const comedor = colegio.comedor ? 'Si' : 'No';
+  const uniforme = colegio.uniforme ? 'Si' : 'No';
+  const calefaccion = colegio.calefaccion ? 'Si' : 'No';
 
   console.log(colegio);
 
   colegio.educationLevels.forEach((level, index) => {
-    educationLevelsString += (index > 0 ? " - " : "") + level.name;
+    educationLevelsString += (index > 0 ? ' - ' : '') + level.name;
   });
 
   colegio.talleres.forEach((taller, index) => {
-    talleresString += (index > 0 ? " - " : "") + taller;
+    talleresString += (index > 0 ? ' - ' : '') + taller;
   });
 
   colegio.religion.forEach((religion, index) => {
-    religionesString += (index > 0 ? " - " : "") + religion;
+    religionesString += (index > 0 ? ' - ' : '') + religion;
   });
 
   return {
@@ -41,6 +41,6 @@ export function index(colegioId) {
     comedor,
     religionesString,
     uniforme,
-    calefaccion,
+    calefaccion
   };
 }
