a07555e11ffcb84566977c8cbe5e5bca3fb2bd06
Cristian Mozetich <cristianmozetich@gmail.com>
2023-12-13 17:58:41 +0000

    conexión login backend

--- frontend/src/components/formLogin/FormLogin.jsx
+++ frontend/src/components/formLogin/FormLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Input } from '@nextui-org/react';
 import Link from 'next/link';
 import { useFormLogin } from '.';
